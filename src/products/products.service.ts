dcdfce5bb6ee9181b3fa090839de133ba33436df
eliezerantonio <eliezerfernandoantonio@gmail.com>
2025-02-15 23:32:31 +0100

    feat: :sparkles: update products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,57 +6,65 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
-export class ProductsService  extends PrismaClient implements OnModuleInit{
+export class ProductsService extends PrismaClient implements OnModuleInit {
 
-  private readonly logger= new Logger("ProudctsServoce")
+  private readonly logger = new Logger("ProudctsServoce")
   onModuleInit() {
-   this.$connect()
-   this.logger.log("Database connected");
-   
+    this.$connect()
+    this.logger.log("Database connected");
+
   }
   create(createProductDto: CreateProductDto) {
-    return this.product.create({data:createProductDto});
+    return this.product.create({ data: createProductDto });
   }
 
-  async findAll(  paginationDto:PaginationDto) {
-    
-
-    const { page, limit}= paginationDto
+  async findAll(paginationDto: PaginationDto) {
 
 
-    const totalPages= await this.product.count()
-    const lastPage = Math.ceil(totalPages/limit)
+    const { page, limit } = paginationDto
 
 
-    return  {
-      data :  await   this.product.findMany({
-          take: limit,
-          skip: (page-1)* limit
-          
-        }), 
-        meta: {
-          total: totalPages, 
-          page, 
-          lastPage
-        }
-      
+    const totalPages = await this.product.count()
+    const lastPage = Math.ceil(totalPages / limit)
+
+
+    return {
+      data: await this.product.findMany({
+        take: limit,
+        skip: (page - 1) * limit
+
+      }),
+      meta: {
+        total: totalPages,
+        page,
+        lastPage
+      }
+
     }
   }
 
   async findOne(id: number) {
-    const product =  await  this.product.findFirst({
-      where: {id}
+    const product = await this.product.findFirst({
+      where: { id }
     })
 
-    if(!product){
+    if (!product) {
 
       throw new NotFoundException(`Product with id ${id} not found`)
     }
     return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+
+    await this.findOne(id)
+
+    return this.product.update({
+
+      where: { id },
+      data: updateProductDto
+    });
   }
 
   remove(id: number) {
